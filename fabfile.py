79731bb4b87ebeb473d419dc5021e9c31938d139
David Farrington <info@davidfarrington.co.uk>
2014-08-01 22:04:59 +0100

    cleanup and prompts

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,33 @@
 from collections import namedtuple
-from datetime import datetime
-from fabric.api import run, env
-from fabric.decorators import task, hosts
+from fabric.api import run, env, abort
+from fabric.decorators import task
 from fabric.context_managers import quiet
 from fabric.colors import green, blue, yellow
+from fabric.contrib.files import exists
+from fabric.operations import prompt
 
 
 class KeysFile(object):
 
     def __init__(self, key_file='~/.ssh/test_authorized_keys'):
-        self.keys = run('cat {}'.format(key_file)).split('\n')
+        if not exists(key_file):
+            abort('SSH key file not found')
+
+        self.key_file = key_file
+        self.keys = [
+            key for key in run('cat {}'.format(key_file)).split('\n') if key
+        ]
         self.users = [self.get_user(key) for key in self.keys]
         self.hashes = [user.hash for user in self.users]
 
     def get_user(self, key):
-        user = namedtuple('User', ['keytype', 'hash', 'name'])
+        user = namedtuple('User', ['keytype', 'hash', 'name', 'full_key'])
+        user.full_key = key
         key_parts = key.split()
+
+        if len(key_parts) < 2 or len(key_parts) > 3:
+            abort('Malformed key: {}'.format(' '.join(key_parts)))
+
         user.keytype = key_parts[0]
         user.hash = key_parts[1]
         user.name = 'anonymous'
@@ -26,61 +38,46 @@ class KeysFile(object):
 
         return user
 
-    def purge_users(self):
-        """
-        Clean authorized_keys except for the given users
-        """
-        allowed_users = [
-            'info@test.co.uk', 'info@other.co.uk',
-            'info@davidfarrington.co.uk'
-        ]
-
-        run(
-            'sed \'/\({hosts}\)/ ! D\' -i-{timestamp}.old {key_file}'.format(
-                hosts='\|'.join(allowed_users),
-                timestamp=datetime.now().strftime('%s'),
-                key_file=self.key_file
-            )
-        )
-
-    def add_user(self, identity_file):
+    def add_user(self, user):
         """
         Add a user to a server using the given identity file
         """
         with quiet():
-            key = run('cat {}'.format(identity_file))
-            new_user = self.get_user(key)
             added = False
 
-            if new_user.hash not in self.hashes:
-                run('echo "{}" >> ~/.ssh/test_authorized_keys'.format(key))
+            if user.hash not in self.hashes:
+                run('echo "{}" >> {}'.format(user.full_key, self.key_file))
                 added = True
 
-            return new_user, added
+            return added
 
-    def remove_user(self, user):
+    def validate_user(self, key):
+        """
+        If passed a key or a path, make a user out of it and return
+        """
+        if exists(key):
+            key = run('cat {}'.format(key))
+        return self.get_user(key)
+
+    def delete_user(self, username):
         """
         Remove a user from a server
         """
         with quiet():
-            run("sed '/{}/ ! D' -i_egg.old ~/.ssh/test_authorized_keys".format(user))
-
-            key = run('cat {}'.format(identity_file))
-            new_user = self.get_user(key)
-            added = False
-
-            if new_user.hash not in self.hashes:
-                run('echo "{}" >> ~/.ssh/test_authorized_keys'.format(key))
-                added = True
-
-            return new_user, added
+            user = [user for user in self.users if user.name == username]
+            if user:
+                run('sed -i \'/{}/d\' {}'.format(user, self.key_file))
+                return True
+            return False
 
 
 @task
-@hosts('david@127.0.0.1')
 def list_users():
     """
-    Read the contents of a servers authorized_keys file
+    Read the contents of a servers test_authorized_keys file
+
+        e.g: fab list_users --hosts david@127.0.0.1
+
     """
     with quiet():
         keyfile = KeysFile()
@@ -91,34 +88,41 @@ def list_users():
 
 
 @task
-@hosts('david@127.0.0.1')
-def add_user(key='~/.ssh/keys/id_rsa.pub'):
+def add_user():
     """
     Add a user to a server using the given identity file
+
+        e.g fab add_user --hosts david@127.0.0.1
+
     """
     with quiet():
         keyfile = KeysFile()
+        user = prompt(green("Paste key or file path:") + "\n\n",
+                      validate=keyfile.validate_user)
 
-    user, added = keyfile.add_user(key)
-
-    if added:
+    if keyfile.add_user(user):
         print(green('{} authorized'.format(user.name, env.host_string)))
     else:
         print(yellow('{} already authorized, skipping'.format(user.name)))
 
 
 @task
-@hosts('david@127.0.0.1')
-def delete_user(user='info@davidfarrington.co.uk'):
+def delete_user(username=None):
     """
     Remove a user from a server
+
+        e.g fab delete_user --hosts david@127.0.0.1
+
     """
     with quiet():
         keyfile = KeysFile()
 
-    user, removed = keyfile.remove_user(user)
+    if not username:
+        username = prompt(green("Username: "))
+
+    removed = keyfile.delete_user(username)
 
     if removed:
-        print(green('{} removed'.format(user, env.host_string)))
+        print(green('{} removed'.format(username, env.host_string)))
     else:
-        print(yellow('{} not in keys, skipping'.format(user)))
+        print(yellow('{} not in keys, skipping'.format(username)))
